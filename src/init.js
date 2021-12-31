dddbc9fa1a3e2e8f9149514572e0143b10c46ade
maxiosare <yoalexgg@gmail.com>
2021-12-31 09:14:56 -0800

    asiasi

---
+++ src/init.js
@@ -0,0 +1,10 @@
+const config = {
+    width: 800,
+    height: 600,
+    parent: "container",
+    type: Phaser.CANVAS,
+    backgroundColor: "#392542"
+    
+}
+
+new Phaser.Game(config);
---
+++ src/sceneA.js
@@ -0,0 +1,8 @@
+class SceneA extends Phaser.Scene {
+    constructor() {
+        super({key: "SceneA"});
+    }
+    create(){
+        alert("se a cargado la escena A")
+    }
+}
